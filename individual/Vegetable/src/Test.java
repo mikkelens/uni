7be0314d95c3dbe2c96e461559656b4da6b02166
Mikkel <Mikkel123sej@gmail.com>
2023-10-03 00:11:28 +0200

    removed individual handouts

--- individual/Vegetable/src/Test.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-import java.net.URISyntaxException;
-
-public class Test {
-    public static void main(String[] args) throws IOException, URISyntaxException {
-        TestServer.testAndOpenInBrowser("vegetable-2");
-    }
-}
